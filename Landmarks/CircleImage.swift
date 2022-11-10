e5d7d00a1c8f919d2397254ea634906f06f21bf3
metteghijsen <mette.ghijsen@gmail.com>
2022-11-10 13:54:36 +0100

    Complete creating and combining views course afgerond. Ik kan nu views en componenten maken, die ik toon op de pagina.

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .frame(width:300)
             .clipShape(Circle())
             .overlay {
             Circle().stroke(.white, lineWidth: 4)
